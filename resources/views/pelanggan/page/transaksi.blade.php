000a9c82ffe61c0c8cc61fb21f44591adad5ee3b
ndvgaindrptr <vega89507@gmail.com>
2024-01-02 07:55:35 +0700

    bug part sekian

--- resources/views/pelanggan/page/transaksi.blade.php
+++ resources/views/pelanggan/page/transaksi.blade.php
@@ -3,11 +3,17 @@
 @section('content')
 <h3 class="mt-3">Keranjang belanja</h3>
 <div class="content mt-3 d-flex flex-lg-wrap gap-4">
-    <div class="card" style="width:200px">
-        <img src="{{asset('assets/images/hitam_brody.png')}}" alt="">
-        <p>Baju Catton warna hitam US</p>
+    <div class="card d-flex flex-col" style="width: 200px;">
+        <img src="{{ asset('assets/images/hitam_brody.png') }}" alt="">
+        <p style="font-size: 24px; font-weight: 700;">Baju Catton warna hitam US</p>
         <h3>Rp. 200.000</h3>
-        <p>Quantity : </p>
+        <p>Quantity :
+            <span class="d-flex align-items-center">
+                <button class="btn btn-secondary" id="plus">+</button>
+                <input type="number" name="qty" id="qty" min="0" max="9999" value="1">
+                <button class="btn btn-secondary" id="minus">-</button>
+            </span>
+        </p>
     </div>
 </div>
 @endsection
