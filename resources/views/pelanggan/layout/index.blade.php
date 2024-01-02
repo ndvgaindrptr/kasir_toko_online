1e203eed7f82ba140477c3570fa95d78e10696c1
ndvgaindrptr <vega89507@gmail.com>
2024-01-02 10:58:07 +0700

    bug lvl up

--- resources/views/pelanggan/layout/index.blade.php
+++ resources/views/pelanggan/layout/index.blade.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="{{asset('css/custome.css')}}">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <title>Toko Online | {{ $title}}</title>
 </head>
 
@@ -40,5 +41,5 @@
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
 </script>
-
+<script src="{{asset('js/custom.js')}}"></script>
 </html>
--- resources/views/pelanggan/page/transaksi.blade.php
+++ resources/views/pelanggan/page/transaksi.blade.php
@@ -1,19 +1,33 @@
 @extends('pelanggan.layout.index')
 
 @section('content')
-<h3 class="mt-3">Keranjang belanja</h3>
-<div class="content mt-3 d-flex flex-lg-wrap gap-4">
-    <div class="card d-flex flex-col" style="width: 200px;">
+<style>
+    input[type="number"]::-webkit-inner-spin-button,
+    input[type="number"]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+</style>
+<h3 class="mt-5">Keranjang belanja</h3>
+<div class="card">
+    <div class="card-body d-flex gap-4">
         <img src="{{ asset('assets/images/hitam_brody.png') }}" alt="">
-        <p style="font-size: 24px; font-weight: 700;">Baju Catton warna hitam US</p>
-        <h3>Rp. 200.000</h3>
-        <p>Quantity :
-            <span class="d-flex align-items-center">
-                <button class="btn btn-secondary" id="plus">+</button>
-                <input type="number" name="qty" id="qty" min="0" max="9999" value="1">
-                <button class="btn btn-secondary" id="minus">-</button>
-            </span>
-        </p>
+        <div class="desc w-100">
+            <p style="font-size:24px; font-weight:700;">Baju Catton warna hitaam US</p>
+            <h3>Rp. 200.000</h3>
+            <div class="row">
+                <label for="qty" class="col-sm-2 col-form-label">Price</label>
+                <div class="col-sm-5 d-flex">
+                    <button class="rounded-start bg-secondary p-2 border border-0" id="plus">+</button>
+                    <input type="number" name="qty" class="form-control w-25 text center" id="qty"
+                    min="0" max="9999" value="1">
+                    <button class="rounded-end bg-secondary p-2 border border-0" id="minus" disabled>-</button>
+                </div>
+            </div>
+            <p>Total :
+                <h4>Rp. 200.000</h4>
+            </p>
+        </div>
     </div>
 </div>
-@endsection
+    @endsection
